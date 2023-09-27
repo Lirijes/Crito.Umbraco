d506da2842558dc3380a4b9d1684b19262e74a70
lirije <lirije11@hotmail.com>
2023-09-27 15:19:30 +0200

    trying to connect my db to umbraco

---
+++ Contexts/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Crito.Models.Enteties;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crito.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        private readonly string _connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\lirij\OneDrive\Skrivbord\crito_local_db\crito_db.mdf;Integrated Security=True;Connect Timeout=30";
+
+        protected DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(_connectionstring);
+        }
+
+        public DbSet<ContactFormEntity> ContactFormEntities { get; set; }
+    }
+}
---
+++ Models/Enteties/ContactFormEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crito.Models.Enteties;
+
+public class ContactFormEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
