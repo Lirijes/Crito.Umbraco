992afa6490ce5abc090308b9254bc77b06c823ff
lirije <lirije11@hotmail.com>
2023-09-29 09:25:17 +0200

    saying i have multipe DbContext trying to restore and remove

---
+++ Models/Entity/ContactFormEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crito.Models.Entity
+{
+    public class ContactFormEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Message { get; set; } = null!;
+    }
+}
---
+++ Models/Entity/NewsletterFormEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crito.Models.Entity
+{
+    public class NewsletterFormEntity
+    {
+        [Key]
+        [Required]
+        public string Email { get; set; } = null!;
+    }
+}
